ae23752380fc82ac9e42f93a769879df6b6a9b97
Dustin Lyons <dlyons14@gmail.com>
2020-08-31 03:08:42 -0700

    Cleanup comments, move Slack App ID out to an env variable

--- updater.js
+++ updater.js
@@ -4,7 +4,7 @@ const shell = require('shelljs');
 const slackTeamName = process.env.SLACK_TEAM_NAME;
 const slackUserName = process.env.SLACK_USER_NAME;
 const slackPassword = process.env.SLACK_PASSWORD;
-const slackAppId = "A016J10PDQF"
+const slackAppId = process.env.SLACK_APP_ID;
 const slackEventsUrl = shell.exec('./print-ngrok-url', {silent: true}).stdout;
 console.log("Found ngrok url... " + slackEventsUrl);
 
@@ -51,39 +51,5 @@ console.log("Found ngrok url... " + slackEventsUrl);
     await page.click('[data-qa="event_request_url"]');
     await page.type('[data-qa="event_request_url"]', slackEventsUrl + '/slack/events');
 
-    //await page.waitForSelector('#retry_url_verification');
-    //await page.click('#retry_url_verification');
-
-    await delay(5000)
-    await page.screenshot({path: 'slack-screenshot.png'});
-
-    /*
-    await page.waitForSelector('#signup');
-    await page.click('#signup');
-    
-    await page.waitForSelector('#s-name');
-    await page.click('#s-name');
-    
-    await page.type('#s-name', 'John');
-    await page.type('#s-surname', 'Doe');
-    await page.type('#s-email', process.env.USER_EMAIL);
-    await page.type('#s-password2', process.env.USER_PASSWORD);
-    await page.type('#s-company', 'John Doe Inc.');
-    
-    await page.waitForSelector('#business');
-    await page.click('#business');
-    
-    await page.waitForSelector('#marketing-agreement');
-    await page.click('#marketing-agreement');
-    
-    await page.waitForSelector('#privacy-policy');
-    await page.click('#privacy-policy');
-    
-    await page.waitForSelector('#register-btn');
-    await page.click('#register-btn');
-    
-    await page.waitForSelector('#login-message', { visible: true });
-    
-    */
     await browser.close();
 })()
